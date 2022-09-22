0e9f0169570c91213c40c9a2eeff7bfc8dc47c7e
tastethedream <tastethedream@gmail.com>
2022-09-22 14:57:43 +0100

    add:ListViewBuilder

--- travelapp/lib/main.dart
+++ travelapp/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/data.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,15 +16,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Travel App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
@@ -40,41 +33,20 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.red.shade900,
         title: const Text("Travel App"),
       ),
-      body: Column
-        (crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         Image.network(
           "https://images.unsplash.com/photo-1452421822248-d4c2b47f0c81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fHRyYXZlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
         ),
-        Container(
+        const SizedBox(
+          height: 50,
+        ),
+        SizedBox(
           height: 100,
-          color: Colors.white70,
-          child: Container(
-            height: 100,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  height: 50.0,
-                  width: 100,
-                  //color: Colors.red.shade900,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.red.shade900,
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "Places",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+              itemCount: dataList.length,
+              itemBuilder: (BuildContext context, index) {
+                return Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
                     height: 50.0,
@@ -83,51 +55,99 @@ class HomePage extends StatelessWidget {
                       borderRadius: BorderRadius.circular(10),
                       color: Colors.red.shade900,
                     ),
-                    child: const Center(
-                      child: Text(
-                        "Hotels",
-                        style: TextStyle(
+                    child: Center(
+                      child: Text(dataList[index].cityName,
+                        style: const TextStyle(
                           fontSize: 18,
                           color: Colors.white,
                         ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 50.0,
-                    width: 100,
-                    //color: Colors.red.shade900,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.red.shade900,
-                    ),
-                    child: const Center(
-                      child: Text(
-                        "Attractions",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
+                );
+              }),
 
-        ),
-        const Padding(
-          padding:  EdgeInsets.all(10.0),
-          child: Text("Popular Destinations",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ) ,),
-        ),
+
+          //     child: ListView.(
+          //       scrollDirection: Axis.horizontal,
+          //       //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //       children: [
+          //         Padding(
+          //           padding: const EdgeInsets.all(8.0),
+          //           child: Container(
+          //             height: 50.0,
+          //             width: 100,
+          //             //color: Colors.red.shade900,
+          //             decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(10),
+          //               color: Colors.red.shade900,
+          //             ),
+          //             child: const Center(
+          //               child: Text(
+          //                 "Places",
+          //                 style: TextStyle(
+          //                   fontSize: 18,
+          //                   color: Colors.white,
+          //                 ),
+          //               ),
+          //             ),
+          //           ),
+          //         ),
+          //         Padding(
+          //           padding: const EdgeInsets.all(8.0),
+          //           child: Container(
+          //             height: 50.0,
+          //             width: 100,
+          //             decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(10),
+          //               color: Colors.red.shade900,
+          //             ),
+          //             child: const Center(
+          //               child: Text(
+          //                 "Hotels",
+          //                 style: TextStyle(
+          //                   fontSize: 18,
+          //                   color: Colors.white,
+          //                 ),
+          //               ),
+          //             ),
+          //           ),
+          //         ),
+          //         Padding(
+          //           padding: const EdgeInsets.all(8.0),
+          //           child: Container(
+          //             height: 50.0,
+          //             width: 100,
+          //             //color: Colors.red.shade900,
+          //             decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(10),
+          //               color: Colors.red.shade900,
+          //             ),
+          //             child: const Center(
+          //               child: Text(
+          //                 "Attractions",
+          //                 style: TextStyle(
+          //                   fontSize: 18,
+          //                   color: Colors.white,
+          //                 ),
+          //               ),
+          //             ),
+          //           ),
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          //
+          //
+          // const Padding(
+          //   padding:  EdgeInsets.all(10.0),
+          //   child: Text("Popular Destinations",
+          //     style: TextStyle(
+          //       fontSize: 20,
+          //       fontWeight: FontWeight.bold,
+          //     ) ,),
+          // ),
+        )
       ]),
     );
   }
